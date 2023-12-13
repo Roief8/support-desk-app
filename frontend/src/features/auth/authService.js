762655de71c8dd62246d5252c022c5cb890f76b2
Roi Efraim <roi.efraim8@gmail.com>
2023-12-13 19:19:26 +0200

    test api with aws

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,12 +1,11 @@
 import axios from "axios"
 
-const BASE_URL =
-  "http://ec2-3-69-231-47.eu-central-1.compute.amazonaws.com/api/users/"
+const BASE_URL = "http://ec2-3-69-231-47.eu-central-1.compute.amazonaws.com"
 const API_URL = "/api/users/"
 
 // Register user
 const register = async (userData) => {
-  const response = await axios.post(BASE_URL, userData)
+  const response = await axios.post(BASE_URL + API_URL, userData)
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(response.data))
   }
@@ -16,7 +15,8 @@ const register = async (userData) => {
 
 // Login user
 const login = async (userData) => {
-  const response = await axios.post(API_URL + "login", userData)
+  console.log(BASE_URL)
+  const response = await axios.post(BASE_URL + API_URL + "login", userData)
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(response.data))
   }
